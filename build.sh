6b0716bc108ed4ccb4a3166ee9bad701b5c25106
akramnameemuddin <akram.dcme@gmail.com>
2025-07-01 21:35:42 +0530

    Remove render.yaml to fix poetry bug

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Running migrations and collecting static files..."
-
 cd backend || exit
-
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
