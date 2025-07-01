9c811c6939e594f5c3ce3cbdd52f930d6109f987
akramnameemuddin <akram.dcme@gmail.com>
2025-07-01 21:08:22 +0530

    Fix build.sh and move requirements.txt to root

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Activating virtual environment..."
-source ../venv/Scripts/activate
-
 echo "Running migrations and collecting static files..."
+
 cd backend || exit
 
 python manage.py makemigrations
