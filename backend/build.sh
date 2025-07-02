c9b1693b9ae8f29ba51b755e6c9773165f49d709
akramnameemuddin <akram.dcme@gmail.com>
2025-07-02 23:19:17 +0530

    Add build.sh deployment script with default templates loading

--- backend/build.sh
+++ backend/build.sh
@@ -3,5 +3,5 @@
 pip install -r ../requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py load_default_templates
+python manage.py create_resume_templates
 python manage.py collectstatic --noinput
