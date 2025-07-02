36806d698b0e3f523eb8bd3232391803148b00e4
akramnameemuddin <akram.dcme@gmail.com>
2025-07-02 10:25:09 +0530

    sucess

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
 
-cd backend || exit
+pip install -r ../requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
