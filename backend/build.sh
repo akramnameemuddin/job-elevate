12ff22958d5558550c002779786eac592320fb70
akramnameemuddin <akramnameemuddin@gmail.com>
2025-08-11 14:00:15 +0530

    need to build for deployment

--- backend/build.sh
+++ backend/build.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
-set -e  # Exit on any error
 
-echo "Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "Checking Django installation..."
-python -c "import django; print('Django version:', django.get_version())"
-
-echo "Running migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-
-echo "Resetting resume templates..."
-python manage.py reset_templates
-
-echo "Collecting static files..."
+pip install -r ../requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py create_resume_templates
 python manage.py collectstatic --noinput
-
-echo "Build completed successfully!"
