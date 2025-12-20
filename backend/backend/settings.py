6d7c79748d43ba02895d2d1f0eb8a20fa1832c78
akramnameemuddin <akramnameemuddin@gmail.com>
2025-12-20 17:24:00 +0530

    production change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import os
 import environ
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Set up environment variables
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,7 +15,6 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = [
-    'job-elevate-m96p.onrender.com',
     'jobelevates.akramnaeemuddin.me',
     'localhost',
     '127.0.0.1',
@@ -108,10 +110,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # DATABASE
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 
 # STATIC/MEDIA
