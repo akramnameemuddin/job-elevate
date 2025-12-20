88a7296a5d4cc4de999c1963c0822fa2d09cbcf4
Akram Naeem Uddin <akram.dcme@gmail.com>
2025-12-20 18:36:15 +0530

    removed unwanted build files

--- backend/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pip install -r ../requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py create_resume_templates
-python manage.py create_admin
-python manage.py collectstatic --noinput
