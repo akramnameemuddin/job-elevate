916184c52aff9a7d4d2e96bc88314294aded57e6
Akram Naeem Uddin <akram.dcme@gmail.com>
2026-03-01 11:28:40 +0530

    Fix: remove ACLs from S3 storage classes (bucket owner enforced)

--- backend/backend/storages.py
+++ backend/backend/storages.py
@@ -3,10 +3,12 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = "static"
-    default_acl = "public-read"
+    default_acl = None
+    querystring_auth = False
 
 
 class MediaStorage(S3Boto3Storage):
     location = "media"
-    default_acl = "public-read"
+    default_acl = None
+    querystring_auth = False
     file_overwrite = False
